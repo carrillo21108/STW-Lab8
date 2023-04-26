79a559579ba70bdcb0d7acd2773dcdbace15fc78
carrillo21108 <87512111+carrillo21108@users.noreply.github.com>
2023-04-25 19:36:23 -0600

    Base del proyecto

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.scss'
+import React from 'react'
+
+function App(){
+    return (
+        <div className="App">
+            <h1>Memoria</h1>
+            <button>Nuevo juego</button>
+        </div>
+    );
+}
+
+export default App
